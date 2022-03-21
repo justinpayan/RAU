932bf5d40da01c31b838e752b78a366f1d452bd7
Justin Payan <jpayan@umass.edu>
2022-03-21 16:26:56 -0400

    parallel query checker

--- query_models.py
+++ query_models.py
@@ -339,7 +339,7 @@ class GreedyMaxQueryModel(QueryModel):
         self.num_procs = num_procs
         # A pool for running the updates in multiple threads
         if num_procs > 1:
-            self.pool = mp.Pool(self.num_procs)
+            self.pool = mp.Pool(processes=self.num_procs)
 
         print("Loading/computing optimal initial solution")
         try:
@@ -417,37 +417,48 @@ class GreedyMaxQueryModel(QueryModel):
             best_q = [x[0] for x in sorted(qry_values.items(), key=lambda x: -x[1])][0]
             return best_q
 
+
+    @staticmethod
+    def check_expected_value(q, reviewer, max_query_val, query_model_object):
+
+        if q in np.where(query_model_object.curr_alloc[reviewer, :])[0].tolist():
+            # print("Update if no")
+            updated_expected_value_if_no, _ = GreedyMaxQueryModel._update_alloc_static(reviewer, q, 0, query_model_object)
+        else:
+            updated_expected_value_if_no = query_model_object.curr_expected_value
+
+        improvement_ub = query_model_object.v_tilde[reviewer, q] * (1 - query_model_object.v_tilde[reviewer, q]) + query_model_object.curr_expected_value
+
+        if improvement_ub < max_query_val or math.isclose(improvement_ub, max_query_val):
+            return query_model_object.curr_expected_value
+        else:
+            updated_expected_value_if_yes, _ = GreedyMaxQueryModel._update_alloc_static(reviewer, q, 1, query_model_object)
+
+            expected_expected_value = query_model_object.v_tilde[reviewer, q] * updated_expected_value_if_yes + \
+                                      (1 - query_model_object.v_tilde[reviewer, q]) * updated_expected_value_if_no
+            # print("Expected expected value of query %d for reviewer %d is %.4f" % (q, reviewer, expected_expected_value))
+            return expected_expected_value
+
     def get_query_parallel(self, reviewer):
+        papers_to_check = set(range(self.n)) - self.already_queried[reviewer]
+
         qry_values = {}
 
-        for q in set(range(self.n)) - self.already_queried[reviewer]:
-            # print("Determine value of %d to %d" % (q, reviewer))
-            # Compute the value of this paper. Return whichever has the best value.
-            # If the paper is not in the current alloc to reviewer, then the alloc won't change if the reviewer bids no
-            # Likewise, if the paper IS in the current alloc, the alloc won't change if the reviewer bids yes.
+        while len(papers_to_check):
+            next_to_check = list(papers_to_check)[:min(self.num_procs, len(papers_to_check))]
+            for p in next_to_check:
+                papers_to_check.remove(p)
 
-            # print("Reviewer %d is currently assigned %s" % (reviewer, np.where(self.curr_alloc[reviewer, :])))
-            # Estimate the improvement in expected value for both answers
-            if q in np.where(self.curr_alloc[reviewer, :])[0].tolist():
-                # print("Update if no")
-                updated_expected_value_if_no, _ = self._update_alloc(reviewer, q, 0)
-            else:
-                updated_expected_value_if_no = self.curr_expected_value
-
-            improvement_ub = self.v_tilde[reviewer, q] * (1 - self.v_tilde[reviewer, q]) + self.curr_expected_value
             max_query_val = max(qry_values.values()) if qry_values else 0
 
-            if qry_values and improvement_ub < max_query_val or math.isclose(improvement_ub, max_query_val):
-                qry_values[q] = self.curr_expected_value
-            else:
-                updated_expected_value_if_yes, _ = self._update_alloc(reviewer, q, 1)
+            list_of_copied_args = [papers_to_check]
+            for argument in [reviewer, max_query_val, self]:
+                list_of_copied_args.append(len(papers_to_check) * [argument])
 
-                expected_expected_value = self.v_tilde[reviewer, q] * updated_expected_value_if_yes + \
-                                          (1 - self.v_tilde[reviewer, q]) * updated_expected_value_if_no
-                # print("Expected expected value of query %d for reviewer %d is %.4f" % (q, reviewer, expected_expected_value))
-                qry_values[q] = expected_expected_value
+            expected_expected_values = self.pool.map(GreedyMaxQueryModel.check_expected_value, zip(*list_of_copied_args))
+            for q, eev in zip(next_to_check, expected_expected_values):
+                qry_values[q] = eev
 
-        # print(sorted(qry_values.items(), key=lambda x: -x[1])[:5], sorted(qry_values.items(), key=lambda x: -x[1])[-5:])
         best_q = [x[0] for x in sorted(qry_values.items(), key=lambda x: -x[1])][0]
         return best_q
 
@@ -682,3 +693,103 @@ class GreedyMaxQueryModel(QueryModel):
         # TODO: Anyway, I think sometimes it isn't even necessary and can be safely omitted.
 
         return updated_expected_value, updated_alloc
+
+    @staticmethod
+    def _update_alloc_static(r, query, response, query_model_object):
+        # We know that if the queried paper is not currently assigned, and its value is 0, the allocation won't change.
+        # print("check value if paper %d for rev %d is %d" % (query, r, response))
+
+        if query_model_object.curr_alloc[r, query] < .1 and response == 0:
+            return query_model_object.curr_expected_value, query_model_object.curr_alloc
+
+        # Otherwise, we need to repeatedly check for augmenting paths in the residual graph
+        # Honestly, I should probably maintain the residual graph at all times
+        # Also, I should first check for augmenting paths coming into/out of the edge we just
+        # queried (oh... or I can just relax basically nm times until I find a negative weight cycle).
+
+        # The residual graph can be represented as a matrix (based on the allocation matrix)
+        # And then I will find an augmenting path by keeping an array that says what the length of the shortest path
+        # is, and an array with the parents for each node.
+
+        # Bipartite graph, with reviewers on left side, papers on right. There is a dummy paper which we will
+        # assign to all reviewers with remaining review load.
+        # We need to have edges with negative v_tilde from paper j to reviewer i when j is assigned to i.
+        # Any unassigned papers have edges from reviewer i to paper j with positive edge weight.
+        # We draw an edge TO the dummy paper when a reviewer has been assigned at least one paper.
+        # We draw an edge FROM the dummy paper when a reviewer still has extra capacity.
+        # TODO: once this whole thing is implemented, I should also make sure that the suggested updates are valid.
+
+        # Use the shortest path faster algorithm to find negative weight cycles, until there aren't any.
+        # https://konaeakira.github.io/posts/using-the-shortest-path-faster-algorithm-to-find-negative-cycles.html
+
+        updated_alloc = query_model_object.curr_alloc
+        res_copy = deepcopy(query_model_object.residual_fwd_neighbors)
+
+        if query_model_object.curr_alloc[r, query] > .5:
+            # update the weight of the edge from query to r (should be positive).
+            res_copy[query + query_model_object.m][r] = response
+        else:
+            # update the weight of the edge from r to query (should be negative).
+            res_copy[r][query + query_model_object.m] = -response
+
+        cycle = True
+        while cycle:
+            cycle = spfa(res_copy)
+
+            if cycle is not None:
+
+                # The cycle goes backward in the residual graph. Thus, we need to assign the i-1'th paper to the i'th
+                # reviewer, and unassign the i+1'th paper.
+                ctr = 0 if cycle[0] < query_model_object.m else 1
+                while ctr < len(cycle):
+                    paper_to_assign = cycle[(ctr - 1) % len(cycle)] - query_model_object.m
+                    paper_to_drop = cycle[(ctr + 1) % len(cycle)] - query_model_object.m
+                    curr_rev = cycle[ctr]
+
+                    if paper_to_assign < query_model_object.n:
+                        # We are assigning a non-dummy paper to the reviewer curr_rev
+                        updated_alloc[curr_rev, paper_to_assign] = 1
+                        # Reverse the edge and negate its weight
+                        res_copy[paper_to_assign + query_model_object.m][curr_rev] = -res_copy[curr_rev][paper_to_assign + query_model_object.m]
+                        del res_copy[curr_rev][paper_to_assign + query_model_object.m]
+
+                    if paper_to_drop < query_model_object.n:
+                        # We are dropping a non-dummy paper from the reviewer curr_rev
+                        updated_alloc[curr_rev, paper_to_drop] = 0
+                        # Reverse the edge and negate its weight
+                        res_copy[curr_rev][paper_to_drop + query_model_object.m] = -res_copy[paper_to_drop + query_model_object.m][curr_rev]
+                        del res_copy[paper_to_drop + query_model_object.m][curr_rev]
+
+                    # Update the residual graph if we have dropped the last paper
+                    # We need to make it so that curr_rev can't receive the dummy paper anymore.
+                    num_papers = np.sum(updated_alloc[curr_rev, :])
+                    if num_papers < 0.1:
+                        try:
+                            del res_copy[curr_rev][query_model_object.n + query_model_object.m]
+                        except KeyError:
+                            pass
+                    # If we have a paper assigned, we can ASSIGN the dummy
+                    else:
+                        res_copy[curr_rev][query_model_object.n + query_model_object.m] = 0
+
+                    # We drop the edge to the dummy paper here if we have assigned the reviewer up to their max.
+                    # So we make it so they can't give away the dummy paper (and thus receive a new assignment).
+                    if num_papers > query_model_object.loads[curr_rev] - .1:
+                        try:
+                            del res_copy[query_model_object.n + query_model_object.m][curr_rev]
+                        except KeyError:
+                            pass
+                    else:
+                        # They can still give away the dummy
+                        res_copy[query_model_object.n + query_model_object.m][curr_rev] = 0
+
+                    # Move to the next REVIEWER... not the next vertex in the cycle
+                    ctr += 2
+
+        # Ok, so now this should be the best allocation. Check the new value of the expected USW, and make sure it
+        # exceeds the value from applying the previous allocation with the new v_tilde.
+        updated_expected_value = np.sum(updated_alloc * query_model_object.v_tilde) - \
+            query_model_object.v_tilde[r, query] * updated_alloc[r, query] + \
+            response * updated_alloc[r, query]
+
+        return updated_expected_value

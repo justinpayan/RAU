5725a502e3a49f14b007c8ef12501c32ce844f54
Justin Payan <jpayan@umass.edu>
2022-03-29 11:26:15 -0400

    script to print out stats

--- collect_outputs.py
+++ collect_outputs.py
@@ -12,6 +12,6 @@ for dset in ["midl"]:
                 expected_usw.append(float(f.read().strip()))
             with open(os.path.join(v_tilde_dir, "true_obj_%s_%s_%d" % (dset, query_model, seed)), 'r') as f:
                 true_usw.append(float(f.read().strip()))
-        print("Dataset: %s, query_model: %s, mean/std True (Exp): %.3f \\pm %.3f (%.3f \\pm %.3f)"
+        print("Dataset: %s, query_model: %s, mean/std True (Exp): $%.2f \\pm %.2f$ ($%.2f \\pm %.2f$)"
               % (dset, query_model, np.mean(true_usw), np.std(true_usw),
                  np.mean(expected_usw), np.std(expected_usw)))

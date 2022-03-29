c1de5ca0a372134d9360cbaee3d8e6d90919bc7d
Justin Payan <jpayan@umass.edu>
2022-03-29 11:23:20 -0400

    script to print out stats

---
+++ collect_outputs.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+
+v_tilde_dir = "/mnt/nfs/scratch1/jpayan/MinimalBidding/v_tildes"
+
+for dset in ["midl"]:
+    for query_model in ["random"]:
+        expected_usw = []
+        true_usw = []
+        for seed in range(10):
+            with open(os.path.join(v_tilde_dir, "expected_obj_%s_%s_%d" % (dset, query_model, seed)), 'r') as f:
+                expected_usw.append(float(f.read().strip()))
+            with open(os.path.join(v_tilde_dir, "expected_obj_%s_%s_%d" % (dset, query_model, seed)), 'r') as f:
+                true_usw.append(float(f.read().strip()))
+        print("Dataset: %s, query_model: %s, mean/std True (Exp): %.3f \\pm %.3f (%.3f \\pm %.3f)"
+              % (dset, query_model, np.mean(true_usw)[0], np.std(true_usw)[0],
+                 np.mean(expected_usw)[0], np.std(expected_usw)[0]))

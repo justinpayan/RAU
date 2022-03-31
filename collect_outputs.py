0316811e1f5222ad99bc3957dee4221b33e830ac
Justin Payan <jpayan@umass.edu>
2022-03-31 10:13:10 -0400

    Directly print table.

--- collect_outputs.py
+++ collect_outputs.py
@@ -4,26 +4,44 @@ import os
 data_dir = "/mnt/nfs/scratch1/jpayan/MinimalBidding"
 v_tilde_dir = "/mnt/nfs/scratch1/jpayan/MinimalBidding/v_tildes"
 
+expected_usw = {}
 init_true_usw = {}
 opt_usw = {}
+dsets = ["midl", "cvpr", "cvpr18"]
+qms = ["random", "tpms", "superstar"]
 
-for dset in ["midl", "cvpr", "cvpr18"]:
-    expected_usw = []
+for dset in dsets:
+    expected_usw_list = []
     init_true_usw_list = []
     opt_usw_list = []
     for seed in range(10):
-        expected_usw.append(np.load(os.path.join(data_dir, "saved_init_expected_usw", "%s_%d.npy" % (dset, seed))))
+        expected_usw_list.append(np.load(os.path.join(data_dir, "saved_init_expected_usw", "%s_%d.npy" % (dset, seed))))
         init_true_usw_list.append(np.load(os.path.join(data_dir, "saved_init_expected_usw", "%s_%d_true.npy" % (dset, seed))))
         opt_usw_list.append(np.load(os.path.join(data_dir, "saved_init_expected_usw", "%s_%d_opt.npy" % (dset, seed))))
 
-    print("Dataset: %s, mean/std True (Exp): $%.2f \\pm %.2f$ ($%.2f \\pm %.2f$), OPT: $%.2f \\pm %.2f$"
-          % (dset, np.mean(init_true_usw_list), np.std(init_true_usw_list),
-             np.mean(expected_usw), np.std(expected_usw), np.mean(opt_usw_list), np.std(opt_usw_list)))
+    # print("Dataset: %s, mean/std True (Exp): $%.2f \\pm %.2f$ ($%.2f \\pm %.2f$), OPT: $%.2f \\pm %.2f$"
+    #       % (dset, np.mean(init_true_usw_list), np.std(init_true_usw_list),
+    #          np.mean(expected_usw_list), np.std(expected_usw_list), np.mean(opt_usw_list), np.std(opt_usw_list)))
     init_true_usw[dset] = init_true_usw_list
     opt_usw[dset] = opt_usw_list
+    expected_usw[dset] = expected_usw_list
 
-for query_model in ["random", "tpms", "superstar"]:
-    for dset in ["midl", "cvpr", "cvpr18"]:
+print("No queries & $%.2f \\pm %.2f$ ($%.2f \\pm %.2f$) & "
+      "$%.2f \\pm %.2f$ ($%.2f \\pm %.2f$) & "
+      "$%.2f \\pm %.2f$ ($%.2f \\pm %.2f$) \\\\" % (np.mean(init_true_usw["midl"]), np.std(init_true_usw["midl"]),
+                                                    np.mean(expected_usw["midl"]), np.std(expected_usw["midl"]),
+                                                    np.mean(init_true_usw["cvpr"]), np.std(init_true_usw["cvpr"]),
+                                                    np.mean(expected_usw["cvpr"]), np.std(expected_usw["cvpr"]),
+                                                    np.mean(init_true_usw["cvpr18"]), np.std(init_true_usw["cvpr18"]),
+                                                    np.mean(expected_usw["cvpr18"]), np.std(expected_usw["cvpr18"]),
+                                                    ))
+
+
+
+str_map = {'random': 'Random', 'tpms': "TPMS", 'superstar': 'SUPER*'}
+for query_model in qms:
+    table_str = str_map[query_model]
+    for dset in dsets:
         expected_usw = []
         true_usw = []
         for seed in range(10):
@@ -34,7 +52,17 @@ for query_model in ["random", "tpms", "superstar"]:
                 tusw = float(f.read().strip())
                 progress = 100.0*(tusw - init_true_usw[dset][seed])/(opt_usw[dset][seed] - init_true_usw[dset][seed])
                 true_usw.append(progress)
-        print("Dataset: %s, query_model: %s, mean/std True Progress (Exp): $%.2f\\%% \\pm %.2f\\%%$ ($%.2f \\pm %.2f$)"
-              % (dset, query_model, np.mean(true_usw), np.std(true_usw),
-                 np.mean(expected_usw), np.std(expected_usw)))
+        table_str += "& $%.2f \\pm %.2f$ ($%.2f \\pm %.2f$) " % \
+                     (np.mean(true_usw), np.std(true_usw), np.mean(expected_usw), np.std(expected_usw))
+    table_str += "\\\\"
+    print(table_str)
+        # print("Dataset: %s, query_model: %s, mean/std True Progress (Exp): $%.2f\\%% \\pm %.2f\\%%$ ($%.2f \\pm %.2f$)"
+        #       % (dset, query_model, np.mean(true_usw), np.std(true_usw),
+        #          np.mean(expected_usw), np.std(expected_usw)))
 
+print("OPT & $%.2f \\pm %.2f$ (NA) & "
+      "$%.2f \\pm %.2f$ (NA) & "
+      "$%.2f \\pm %.2f$ (NA) \\\\" % (np.mean(opt_usw["midl"]), np.std(opt_usw["midl"]),
+                                        np.mean(opt_usw["cvpr"]), np.std(opt_usw["cvpr"]),
+                                        np.mean(opt_usw["cvpr18"]), np.std(opt_usw["cvpr18"]),
+                                        ))

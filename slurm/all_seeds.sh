f91fdae9bae470f33246c83b4ee24a2b8588ba72
Justin Payan <jpayan@umass.edu>
2022-04-21 12:14:32 -0400

    Run exp with random model.

--- slurm/all_seeds.sh
+++ slurm/all_seeds.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+MODEL=random
+
 for SEED in {0..9}; do
-  ./repeatedExps.sbatch $SEED midl supergreedymax
-  ./repeatedExps.sbatch $SEED cvpr supergreedymax
-  ./repeatedExps.sbatch $SEED cvpr18 supergreedymax
+  ./repeatedExps.sbatch $SEED midl $MODEL
+  ./repeatedExps.sbatch $SEED cvpr $MODEL
+  ./repeatedExps.sbatch $SEED cvpr18 $MODEL
 done

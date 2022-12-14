17e8c5adf7c2d4a0985f2c82c58486118cabe3bd
Justin Payan <jpayan@umass.edu>
2022-12-14 11:01:16 -0500

    Save logs to unique filenames.

--- slurm/batchICLRTests.sh
+++ slurm/batchICLRTests.sh
@@ -6,7 +6,8 @@ LOG_DIR=/mnt/nfs/scratch1/jpayan/logs/MinimalBidding
 for SEED in {0..9}; do
   for YEAR in {2018..2022}; do
     sbatch --nodelist=swarm082 --time=00-12:00:00 --partition=defq \
-    --nodes=1 --ntasks=1 --output=$LOG_DIR/iclr_tests.out --error=$LOG_DIR/iclr_tests.err --job-name=iclr_tests \
+    --nodes=1 --ntasks=1 --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}.out \
+    --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}.err --job-name=iclr_tests_${YEAR}_${SEED} \
     ./runICLRTests.sh $DATA_DIR $SEED $YEAR
   done
 done

ae0ab3e116648872689ea544b289e56306ec5dbc
Justin Payan <jpayan@umass.edu>
2022-12-15 10:15:33 -0500

    Ask for more time and memory for jobs.

--- slurm/batchICLRTests.sh
+++ slurm/batchICLRTests.sh
@@ -5,8 +5,8 @@ LOG_DIR=/mnt/nfs/scratch1/jpayan/logs/MinimalBidding
 
 for SEED in {0..9}; do
   for YEAR in {2018..2022}; do
-    sbatch --nodelist=swarm082 --time=00-12:00:00 --partition=defq \
-    --nodes=1 --ntasks=1 --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}.out \
+    sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+    --nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}.out \
     --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}.err --job-name=iclr_tests_${YEAR}_${SEED} \
     ./runICLRTests.sh $DATA_DIR $SEED $YEAR
   done

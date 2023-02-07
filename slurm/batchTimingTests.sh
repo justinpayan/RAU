0a4550ba9d9c0d0263392be241badd5edaccaedd
Justin Payan <jpayan@umass.edu>
2023-02-07 11:50:52 -0500

    need to run a bash script with sbatch

---
+++ slurm/batchTimingTests.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+DATA_DIR=/mnt/nfs/scratch1/jpayan/MinimalBidding
+LOG_DIR=/mnt/nfs/scratch1/jpayan/logs/MinimalBidding
+
+SEED=0
+YEAR=2020
+
+sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+--nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.out \
+--error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.err --job-name=iclr_tests_${YEAR}_${SEED}_true_true \
+./runTimingTests.sh $DATA_DIR $SEED $YEAR --caching --dykstra
+
+sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+--nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.out \
+--error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.err --job-name=iclr_tests_${YEAR}_${SEED}_true_false \
+./runTimingTests.sh $DATA_DIR $SEED $YEAR --caching --no-dykstra
+
+sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+--nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.out \
+--error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.err --job-name=iclr_tests_${YEAR}_${SEED}_false_true \
+./runTimingTests.sh $DATA_DIR $SEED $YEAR --no-caching --dykstra
+
+sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+--nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.out \
+--error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.err --job-name=iclr_tests_${YEAR}_${SEED}_false_false \
+./runTimingTests.sh $DATA_DIR $SEED $YEAR --no-caching --no-dykstra
+
--- slurm/runTimingTests.sh
+++ slurm/runTimingTests.sh
@@ -1,28 +1,9 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-DATA_DIR=/mnt/nfs/scratch1/jpayan/MinimalBidding
-LOG_DIR=/mnt/nfs/scratch1/jpayan/logs/MinimalBidding
+DATA_DIR=$1
+SEED=$2
+YEAR=$3
+CACHESTR=$4
+DYKSTR=$5
 
-SEED=0
-YEAR=2020
-
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.out \
---error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.err --job-name=iclr_tests_${YEAR}_${SEED}_true_true \
-python ../timing_exps.py --data_dir $DATA_DIR --seed $SEED --year $YEAR --caching --dykstra
-
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.out \
---error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.err --job-name=iclr_tests_${YEAR}_${SEED}_true_false \
-python ../timing_exps.py --data_dir $DATA_DIR --seed $SEED --year $YEAR --caching --no-dykstra
-
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.out \
---error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.err --job-name=iclr_tests_${YEAR}_${SEED}_false_true \
-python ../timing_exps.py --data_dir $DATA_DIR --seed $SEED --year $YEAR --no-caching --dykstra
-
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.out \
---error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.err --job-name=iclr_tests_${YEAR}_${SEED}_false_false \
-python ../timing_exps.py --data_dir $DATA_DIR --seed $SEED --year $YEAR --no-caching --no-dykstra
-
+python ../timing_exps.py --data_dir $DATA_DIR --seed $SEED --year $YEAR $CACHESTR $DYKSTR

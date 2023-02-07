17e58b2755f433bc5322bf5ef8c610268b377a49
Justin Payan <jpayan@umass.edu>
2023-02-07 12:07:56 -0500

    run on 074

--- slurm/batchTimingTests.sh
+++ slurm/batchTimingTests.sh
@@ -6,22 +6,22 @@ LOG_DIR=/mnt/nfs/scratch1/jpayan/logs/MinimalBidding
 SEED=0
 YEAR=2020
 
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+sbatch --nodelist=swarm074 --time=05-12:00:00 --partition=longq \
 --nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.err --job-name=iclr_tests_${YEAR}_${SEED}_true_true \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --caching --dykstra
 
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+sbatch --nodelist=swarm074 --time=05-12:00:00 --partition=longq \
 --nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.err --job-name=iclr_tests_${YEAR}_${SEED}_true_false \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --caching --no-dykstra
 
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+sbatch --nodelist=swarm074 --time=05-12:00:00 --partition=longq \
 --nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.err --job-name=iclr_tests_${YEAR}_${SEED}_false_true \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --no-caching --dykstra
 
-sbatch --nodelist=swarm082 --time=05-12:00:00 --partition=longq \
+sbatch --nodelist=swarm074 --time=05-12:00:00 --partition=longq \
 --nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.err --job-name=iclr_tests_${YEAR}_${SEED}_false_false \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --no-caching --no-dykstra

e826ceaa77063584a80ecfd5a45157bbe6c9a14a
Justin Payan <jpayan@umass.edu>
2023-02-13 12:18:17 -0500

    Ask for less mem

--- slurm/batchTimingTests.sh
+++ slurm/batchTimingTests.sh
@@ -10,22 +10,22 @@ NODELIST=""
 TIME="--time=00-1:00:00"
 
 sbatch $NODELIST $TIME --partition=$PARTITION \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.out \
+--nodes=1 --ntasks=1 --mem=10G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_true.err --job-name=iclr_tests_${YEAR}_${SEED}_true_true \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --caching --dykstra
 
 sbatch $NODELIST $TIME --partition=$PARTITION \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.out \
+--nodes=1 --ntasks=1 --mem=10G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_true_false.err --job-name=iclr_tests_${YEAR}_${SEED}_true_false \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --caching --no-dykstra
 
 sbatch $NODELIST $TIME --partition=$PARTITION \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.out \
+--nodes=1 --ntasks=1 --mem=10G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_true.err --job-name=iclr_tests_${YEAR}_${SEED}_false_true \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --no-caching --dykstra
 
 sbatch $NODELIST $TIME --partition=$PARTITION \
---nodes=1 --ntasks=1 --mem=40G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.out \
+--nodes=1 --ntasks=1 --mem=10G --output=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.out \
 --error=$LOG_DIR/iclr_tests_${YEAR}_${SEED}_false_false.err --job-name=iclr_tests_${YEAR}_${SEED}_false_false \
 ./runTimingTests.sh $DATA_DIR $SEED $YEAR --no-caching --no-dykstra
 

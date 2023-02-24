f4a9ea5abc708469fdd1bd52a777aa9927df5176
Justin Payan <jpayan@umass.edu>
2023-02-24 13:46:32 -0500

    int reqd

--- solve_usw.py
+++ solve_usw.py
@@ -77,7 +77,7 @@ def solve_usw_gurobi(affinity_scores, covs, loads):
     env = gp.Env(empty=True)
     env.setParam('WLSAccessID', 'a874a18f-8070-4ce8-b5c3-206c3625d8a6')
     env.setParam('WLSSecret', 'e685b441-d07f-43f4-aaeb-5e6385a6bc07')
-    env.setParam('LicenseID', '937238')
+    env.setParam('LicenseID', 937238)
     env.start()
 
     paper_rev_pairs, pras = create_multidict(affinity_scores)
